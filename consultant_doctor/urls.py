a8df731a1147a67318479609d1820c314b7b94a1
sriramreddy-7 <sriramreddykoonadi@gmail.com>
2023-07-02 10:58:12 +0530

    1st commit to deploy in Railway

---
+++ consultant_doctor/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+app_name = 'consultant_doctor'
+
+urlpatterns = [
+
+    path('consultantDoctor_dashboard',views.consultantDoctor_dashboard,name='consultantDoctor_dashboard'),
+    path('logout_view',views.logout_view,name="logout_view"),
+    path('consultantDoctor_patientList',views.consultantDoctor_patientList,name="consultantDoctor_patientList"),
+    path('consultantDoctor_appointmentList',views.consultantDoctor_appointmentList,name="consultantDoctor_appointmentList"),
+    path('consultantDoctor_patientDiagonise/<str:patient_id>/',views.consultantDoctor_patientDiagonise,name="consultantDoctor_patientDiagonise"),
+    path('consultantDoctor_patientDiagonise_View_Edit/<str:patient_id>/',views.consultantDoctor_patientDiagonise_View_Edit,name="consultantDoctor_patientDiagonise_View_Edit"),
+
+
+
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
