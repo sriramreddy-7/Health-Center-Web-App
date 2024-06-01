80f57f89216bb23cb4a018b68685872fb88ca5d9
sriramreddy-7 <sriramreddykoonadi@gmail.com>
2024-06-01 16:32:09 +0530

    added new patient files

---
+++ patient/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from patient.models import PatientPrimaryData,FT,RP,PHR,Visit,JDD,Test,MedicalTestResult,TestForm,PatientTest,TestReport,Medicine
+from patient.models import PatientPrescription,PrescribedMedicine
+# Register your models here.
+admin.site.register(PatientPrimaryData)
+# admin.site.register(PatientCount)
+# admin.site.register(Appointment)
+admin.site.register(Visit)
+admin.site.register(FT)
+admin.site.register(RP)
+admin.site.register(PHR)
+admin.site.register(JDD)
+admin.site.register(Test)
+admin.site.register(MedicalTestResult)
+admin.site.register(TestForm)
+admin.site.register(PatientTest)
+admin.site.register(TestReport)
+admin.site.register(Medicine)
+admin.site.register(PatientPrescription)
+admin.site.register(PrescribedMedicine)
